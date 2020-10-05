6dbdb1cd4f6a0dfb697fc1d8d4f34cbb4e7e100c
Florian Leroux <lerouxflorian1@gmail.com>
2020-10-06 00:14:24 +0200

    refactor: refonte en virtual dom selon vdom

--- packages/quantum/src/main.ts
+++ packages/quantum/src/main.ts
@@ -1,5 +1,59 @@
+import createElement from "./app/vdom/createElement";
+import diff from "./app/vdom/diff";
+import mount from "./app/vdom/mount";
+import render from "./app/vdom/render";
+
+import { div, span } from './app/vdom/elements';
+
+const createVApp = (timer: { hours: number; minutes: number; seconds: number; }) => div({
+    attrs: {
+        id: 'app',
+    },
+    children: [
+        span({
+            children: [
+                String(timer.hours),
+                'h '
+            ],
+        }),
+        span({
+            children: [
+                String(timer.minutes),
+                'm '
+            ],
+        }),
+        span({
+            children: [
+                String(timer.seconds),
+                's'
+            ],
+        }),
+    ]
+});
+
+const timer = () => {
+    const now = new Date();
+
+    return {
+        hours: now.getHours(),
+        minutes: now.getMinutes(),
+        seconds: now.getSeconds(),
+    };
+};
+
+let vApp = createVApp(timer());
+const $app = render(vApp);
+const $root = document.getElementById('root');
+let $rootEl = mount($app, $root);
+
 function bootstrap(): void {
-    const root = document.getElementById('root');
+    setInterval(() => {
+        const vNewApp = createVApp(timer());
+        const patch = diff(vApp, vNewApp);
+        $rootEl = patch($rootEl);
+
+        vApp = vNewApp;
+    }, 1000);
 }
 
 bootstrap();
